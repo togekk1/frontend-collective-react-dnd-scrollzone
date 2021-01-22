670c47c03ae3cc26ca07fcf31dfa0c51d4b5a72a
Ben Tea <ben@shippabo.com>
2021-01-21 17:47:51 -0800

    remove unneeded import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { findDOMNode } from 'react-dom';
 import throttle from 'lodash.throttle';
 import raf from 'raf';
 import getDisplayName from 'react-display-name';
